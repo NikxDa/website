47d01513b44850c0e614f6b0cf131e19fccf5b17
NikxDa <NikxDa@users.noreply.github.com>
2018-12-08 12:20:28 +1100

    Add project status

--- src/blocks/Project/ProjectStatus.js
+++ src/blocks/Project/ProjectStatus.js
@@ -1,5 +1,24 @@
+import React from "react";
 import styled from "styled-components";
 
-export default styled.div`
-    
+const StatusName = styled.p`
+    display: block;
+    text-transform: uppercase;
+    font-size: .8rem;
+    letter-spacing: 1px;
+    color: #959EB2;
+    margin-bottom: .25rem;
 `;
+
+const StatusValue = styled.p`
+    display: block;
+    font-size: 1rem;
+    color: #666C7A;
+`;
+
+export default (props) => (
+    <div>
+        <StatusName>{props.title}</StatusName>
+        <StatusValue>{props.value}</StatusValue>
+    </div>
+)
