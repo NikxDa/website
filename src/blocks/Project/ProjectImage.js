f0364f26c55248e8c2798705f5bb5c5c0e4c96f2
NikxDa <NikxDa@users.noreply.github.com>
2018-12-10 23:35:20 +1100

    Remove ProjectImage

--- src/blocks/Project/ProjectImage.js
+++
@@ -1,6 +0,0 @@
-import styled from "styled-components";
-import { darkOrange } from "../../utils/themeColors";
-
-export default styled.img`
-    width: ${props => props.imageWidth || "50"}%;
-`;
--- src/blocks/Project/index.js
+++ src/blocks/Project/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 import Flex from "../../elements/Flex";
-import Transform from "../../elements/Transform";
 import Margin from "../../elements/Margin";
-import Reveal from "../../elements/Reveal";
-
-import ProjectImage from "./ProjectImage";
 import ProjectTitle from "./ProjectTitle";
 import ProjectDescription from "./ProjectDescription";
 import ProjectType from "./ProjectType";
@@ -14,39 +10,20 @@ import ProjectStatus from "./ProjectStatus";
 export default ({
     title,
     description,
-    image,
     type,
     status,
     releaseDate,
-    imageWidth,
-    imageOrientation,
-    imageTranslateX,
-    imageTranslateY,
-    imageTranslateRotate
+    ...props
 }) => (
-    <Flex direction="row" justifyContent="space-around" alignItems="center">
-        { imageOrientation !== "r" && 
-            <Transform translateX={imageTranslateX} translateY={imageTranslateY} rotate={imageTranslateRotate}>
-                <ProjectImage imageWidth={imageWidth} src={image} />
-            </Transform>
-        }
-
-        <div style={{width: "40%"}}>
-            <ProjectType>{type}</ProjectType>
-            <ProjectTitle>{title}</ProjectTitle>
-            <ProjectDescription>{description}</ProjectDescription>
-            <Margin top="1rem">
-                <Flex justifyContent="space-between">
-                    <ProjectStatus title="Status" value={status} />
-                    <ProjectStatus title="Release" value={releaseDate} />
-                </Flex>
-            </Margin>
-        </div>
-
-        { imageOrientation === "r" && 
-            <Transform translateX={imageTranslateX} translateY={imageTranslateY} rotate={imageTranslateRotate}>
-                <ProjectImage imageWidth={imageWidth} src={image} />
-            </Transform>
-        }
-    </Flex>
+    <div {...props}>
+        <ProjectType>{type}</ProjectType>
+        <ProjectTitle>{title}</ProjectTitle>
+        <ProjectDescription>{description}</ProjectDescription>
+        <Margin top="1rem">
+            <Flex justifyContent="space-between">
+                <ProjectStatus title="Status" value={status} />
+                <ProjectStatus title="Release" value={releaseDate} />
+            </Flex>
+        </Margin>
+    </div>
 );
