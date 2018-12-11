a58fac6055d966a3141ee14b0518c254d066caa5
NikxDa <NikxDa@users.noreply.github.com>
2018-12-11 13:24:50 +1100

    Update Transform component

--- src/elements/Transform.js
+++ src/elements/Transform.js
@@ -1,12 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-export default styled(({ children, className}) => {
-    return React.cloneElement(React.Children.only(children), {
-        className: `${children.props.className} ${className}`.trim()
+export default styled(({ children, className }) => {
+    return React.cloneElement (React.Children.only (children), {
+        className: `${children.props.className} ${className}`.trim ()
     })
 })`
-    position:       relative;
     transform: ${props =>
         ["translateX", "translateY", "translateZ", "translate", "rotate", "scale"]
             .map (itm => props [itm] ? `${itm}(${props[itm]})` : null)
