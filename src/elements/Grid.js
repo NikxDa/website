9d933278919edc1433d607bfdaec975059497cc7
NikxDa <NikxDa@users.noreply.github.com>
2018-12-15 19:52:51 +1100

    Fix linter errors

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -1,9 +1,8 @@
 import styled from "styled-components";
-import media from "../utils/mediaQueries";
 
 export default styled.div`
     display: grid;
-    grid-template-columns: ${props => props.areas ? `repeat(${props.areas [0].split (" ").length}, 1fr)` : (props.columnDefs ? props.columnDefs : `repeat(${props.columns || 2}, 1fr)`)};
+    grid-template-columns: ${props => props.areas ? `repeat(${props.areas[0].split (" ").length}, 1fr)` : (props.columnDefs ? props.columnDefs : `repeat(${props.columns || 2}, 1fr)`)};
     row-gap: ${props => props.verticalSpacing || "0"};
     column-gap: ${props => props.horizontalSpacing || "0"};
     align-items: ${props => props.alignItems || "flex-start"};
